5dffd11c9ca9cf0fa611545ffabdc30a279c7cb4
Kevin Mazzarella <jitcola13@gmail.com>
2019-07-09 14:54:43 -0500

    Progress on reading in .job files

--- JobProcessing/processor.cpp
+++ JobProcessing/processor.cpp
@@ -2,8 +2,12 @@
 #include <filesystem>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <string>
 
 void processJob(int jobId);
+
+std::string stripDataLabel(std::string line);
 
 int main(void)
 {
@@ -13,10 +17,80 @@ int main(void)
 
 void processJob(int jobId)
 {
-    int id {-1};
-    std::string precision {""};
-    std::string multiprecisionBits {""};
-    std::string sampleRate {""};
+    std::string currentLine;
+    std::string standardSeparator{": "};
+    std::string::size_type index;
+    std::string data;
+    int id {jobId};
+    bool doublePrecision;
+    int multiprecisionBits;
+    int sampleRate;
+    int length;
     std::vector<int> externalModOutDependencies;
-    std::vector<int> externalModInDependencies;    
+    std::vector<int> externalModInDependencies;
+
+    std::ifstream jobFile {"Job" + std::to_string(jobId) + ".job"};
+    
+    getline(jobFile, currentLine);
+    data = stripDataLabel(currentLine);
+    id = std::stoi(data);
+    getline(jobFile, currentLine);
+    data = stripDataLabel(currentLine);
+    doublePrecision = data == " double" ? true : false;
+    getline(jobFile, currentLine);
+    data = stripDataLabel(currentLine);
+    multiprecisionBits = std::stoi(data);
+    getline(jobFile, currentLine);
+    data = stripDataLabel(currentLine);
+    sampleRate = std::stoi(data);
+    getline(jobFile, currentLine);
+    data = stripDataLabel(currentLine);
+    length = std::stoi(data);
+    getline(jobFile, currentLine);
+    data = stripDataLabel(currentLine);
+    if (data != " ") {
+        if (data.find(',') == std::string::npos) {
+            externalModOutDependencies.push_back(stoi(data));
+        } else {
+            std::string::size_type nextCommaIndex = data.find(',');
+            while (nextCommaIndex != std::string::npos) {
+                externalModOutDependencies.push_back(stoi(data.substr(static_cast<std::string::size_type>(0), nextCommaIndex)));
+                data = data.substr(nextCommaIndex + static_cast<std::string::size_type>(1));
+                nextCommaIndex = data.find(',');
+            }
+            externalModOutDependencies.push_back(stoi(data));
+        }
+    }
+    getline(jobFile, currentLine);
+    data = stripDataLabel(currentLine);
+    if (data != " ") {
+        if (data.find(',') == std::string::npos) {
+            externalModInDependencies.push_back(stoi(data));
+        } else {
+            std::string::size_type nextCommaIndex = data.find(',');
+            while (nextCommaIndex != std::string::npos) {
+                externalModInDependencies.push_back(stoi(data.substr(static_cast<std::string::size_type>(0), nextCommaIndex)));
+                data = data.substr(nextCommaIndex + static_cast<std::string::size_type>(1));
+                nextCommaIndex = data.find(',');
+            }
+            externalModInDependencies.push_back(stoi(data));
+        }
+    }
+            
+    std::cout << id << "\n"
+              << doublePrecision << "\n"
+              << multiprecisionBits << "\n"
+              << sampleRate << "\n"
+              << length << "\n";
+    for (int modId: externalModOutDependencies)
+        std::cout << modId << " ";
+    std::cout << "\n";
+    for (int modId: externalModInDependencies)
+        std::cout << modId << " ";
+    std::cout << "\n";
+}
+
+std::string stripDataLabel(std::string line)
+{
+    return line.substr(line.find(": ") + static_cast<std::string::size_type>(1));
 }
